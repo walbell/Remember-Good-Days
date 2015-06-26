db788471543b147721792a181cbeab6b2a6d2f8d
Walter Bellante <walbell88@hotmail.it>
2015-06-26 16:59:03 +0100

    Added logic for conditionally reload data if more than 1 day has passed

--- js/services/feed.js
+++ js/services/feed.js
@@ -2,7 +2,8 @@
 	var feedService = function($q, $http, userService) {
 
 		var instagramServerAPI = 'https://api.instagram.com/v1/',
-			user_media_list = [];
+			user_media_list = [],
+			MAX_TIME_BEFORE_REFRESH = 1000*60*60*24; //1 day 
 
 		return {
 			getUserMedia: function() {
@@ -10,7 +11,9 @@
 				var access_token = userService.getUserToken(),
 					user_id = userService.getUserId(),
 					endpoint = instagramServerAPI + 'users/' + user_id + '/media/recent?access_token=' + access_token + '&callback=JSON_CALLBACK&count=50',
-					deferred = $q.defer();
+					deferred = $q.defer(),
+					date_last_access = Date.parse(userService.getCurrentUserTimestamp()),
+					date_current_access = new Date(); 
 
 				function getRecentMedia (URL, count){
 					$http.jsonp(URL.replace(/angular.callbacks._\d/,'JSON_CALLBACK')).success(function(response) {
@@ -21,23 +24,26 @@
 							user_media_list.push(response.data[i]);
 						}
 
-						if (response.pagination.next_url && count <= 5) {
+						if (response.pagination.next_url && count <= 5 ) {
 						   count = count + 1;
 	                       getRecentMedia(response.pagination.next_url, count);
 						}
 						else {
 							userService.storeUserFeed(user_media_list);
 							userService.storeCurrentUserTimestamp();
-							console.log('user_feed',userService.getUserFeed());
-							console.log('timestamp',userService.getCurrentUserTimestamp());
-							var data = new Date();
-							console.log('difference ', data - Date.parse(userService.getCurrentUserTimestamp()));
 							deferred.resolve(user_media_list);
 						}
                 	});
                 }
 
-                getRecentMedia(endpoint, 0);
+                if (date_current_access - date_last_access > MAX_TIME_BEFORE_REFRESH)
+                {
+	                getRecentMedia(endpoint, 0);
+                }
+                else
+                {
+                	deferred.resolve(userService.getUserFeed());
+                }
 
 				return deferred.promise;
 			}
--- www/js/index.js
+++ www/js/index.js
@@ -245,7 +245,8 @@ require('angular-local-storage');
 	var feedService = function($q, $http, userService) {
 
 		var instagramServerAPI = 'https://api.instagram.com/v1/',
-			user_media_list = [];
+			user_media_list = [],
+			MAX_TIME_BEFORE_REFRESH = 1000*60*60*24; //1 day 
 
 		return {
 			getUserMedia: function() {
@@ -253,7 +254,9 @@ require('angular-local-storage');
 				var access_token = userService.getUserToken(),
 					user_id = userService.getUserId(),
 					endpoint = instagramServerAPI + 'users/' + user_id + '/media/recent?access_token=' + access_token + '&callback=JSON_CALLBACK&count=50',
-					deferred = $q.defer();
+					deferred = $q.defer(),
+					date_last_access = Date.parse(userService.getCurrentUserTimestamp()),
+					date_current_access = new Date(); 
 
 				function getRecentMedia (URL, count){
 					$http.jsonp(URL.replace(/angular.callbacks._\d/,'JSON_CALLBACK')).success(function(response) {
@@ -264,23 +267,26 @@ require('angular-local-storage');
 							user_media_list.push(response.data[i]);
 						}
 
-						if (response.pagination.next_url && count <= 5) {
+						if (response.pagination.next_url && count <= 5 ) {
 						   count = count + 1;
 	                       getRecentMedia(response.pagination.next_url, count);
 						}
 						else {
 							userService.storeUserFeed(user_media_list);
 							userService.storeCurrentUserTimestamp();
-							console.log('user_feed',userService.getUserFeed());
-							console.log('timestamp',userService.getCurrentUserTimestamp());
-							var data = new Date();
-							console.log('difference ', data - Date.parse(userService.getCurrentUserTimestamp()));
 							deferred.resolve(user_media_list);
 						}
                 	});
                 }
 
-                getRecentMedia(endpoint, 0);
+                if (date_current_access - date_last_access > MAX_TIME_BEFORE_REFRESH)
+                {
+	                getRecentMedia(endpoint, 0);
+                }
+                else
+                {
+                	deferred.resolve(userService.getUserFeed());
+                }
 
 				return deferred.promise;
 			}
